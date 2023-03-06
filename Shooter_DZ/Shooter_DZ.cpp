9ef18f5da38bef2d23398c8105824640aaa0bebe
Мария Тарлавина <tarlavina50@mail.ru>
2023-03-06 21:14:35 +0700

    Encoding

---
+++ Shooter_DZ/Shooter_DZ.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<conio.h>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	char key;
+	do
+	{
+		key = _getch();
+		if (key == 119)
+			cout << key << "\tВверх\n";
+		else if (key == 97)
+			cout << key << "\tВлево\n";
+		else if (key == 115)
+			cout << key << "\tВниз\n";
+		else if (key == 100)
+			cout << key << "\tВправо\n";
+		else if (key == 32)
+			cout << "space" << "\tПрыжок\n";
+		else if (key == 13)
+			cout << "enter" << "\tОгонь\n";
+		else if (key == 27)
+			cout << "escape" << "\tВыход\n";
+	} while (key != 27);
+}
